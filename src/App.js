e503ef237fc91a7edc7d233991140cd503e73597
Matheus Holstein Pires <themathpires@gmail.com>
2021-10-31 11:02:05 +0000

    api request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React from 'react';
 import './App.css';
 import Pokedex from 'pokedex-promise-v2';
 
-const P = new Pokedex();
+const pokedex = new Pokedex();
+const interval = {
+  limit: 10,
+  offset: 0
+}
 
-console.log(P);
+pokedex.getPokemonsList(interval) 
+.then(function(response) {
+  localStorage.setItem('pokemonsList', JSON.stringify(response.results));
+})
+.catch(function(error) {
+  console.log('There was an ERROR: ', error);
+});
 
-P.getPokemonsList() 
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log('There was an ERROR: ', error);
-    });
+const allPokemons = JSON.parse(localStorage.getItem('pokemonsList'));
+const pokemonNames = allPokemons.map((pokemon) => { return pokemon.name; });
+
+console.log(pokemonNames);
 
 function App() {
   return (
     <div className="App">
       <ul>
-        
+        {pokemonNames.map(name => {          
+          <li>{name}</li>
+        })}
       </ul>
     </div>
   );
